83ed91783b73918cf013c09da5bc66ad10a83df3
Chris Maxwell <chmaxwel@icloud.com>
2020-08-13 14:22:32 +0200

    updated app.use

--- server.js
+++ server.js
@@ -10,8 +10,40 @@ const db = new sqlite3.Database('./db/election.db', err => {
     if (err) {
         return console.error(err.message);
     }
-
     console.log('Connected to the election database');
+});
+
+// DB response all array
+db.all(`SELECT * FROM candidates`, (err, rows) => {
+    //console.log(rows);
+});
+
+//DB for single id in array
+// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id =?`, 1, function(err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID);
 });
 
 // Default response for any other request(Not Found) Catch all
