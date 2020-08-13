96daaa4782abc246a34dbd515d6f95a754eb61b2
Chris Maxwell <chmaxwel@icloud.com>
2020-08-13 12:52:51 +0200

    updated db to start before server

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connected to the election database');
+});
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start Server after db connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
